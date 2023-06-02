435633c2baad7edd317c484e4cbf9bc6e75fad4a
Isabel Zhong <isabelzhong30@gmail.com>
2023-06-02 13:51:30 -0500

    wip

---
+++ src/LPBus.cpp
@@ -0,0 +1,10 @@
+#include <Arduino.h>
+#include <LPBus.h>
+
+LPBus::Initialize()
+{
+    // initialize bus
+#if defined(ARDUINO_TEENSY40) || defined(ARDUINO_TEENSY41)
+    // register all messages
+#endif
+}
